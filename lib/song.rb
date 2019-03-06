0b3cfc2580faed73ce2e1dd65240086bde6c228e
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-06 04:32:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,16 @@
 #This is our 'joning' model between our Artist and our Genre classes
 
 class Song
+  attr_accessor :name, :artist, :genre
 
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
 
 
 end
