4b7a14b8c18daeb290495e92bfde83a19e0ebdd9
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-06 03:59:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,23 @@
 class Artist
-  attr_accessor :name, :genre
+  attr_accessor :name
 
-  @@all = []
-  def initialize (name)
+  def initialize(name)
+    @songs = []
     @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def new_song(name, genre)
-    Song.new(name, genre)
   end
 
   def songs
     @songs
   end
 
-  def genres
-
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
 
+  def genres                        #=> This is how the other two
+    self.songs.collect do |song|    #=> classes talk to each other
+      song.genre
+    end
+  end
 end
