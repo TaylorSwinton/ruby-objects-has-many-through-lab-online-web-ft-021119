71457edc8cad788f745a9f9ce3fd6dbc788047c7
Taylor Swinton <taylorswinton1@gmail.com>
2019-03-06 05:30:42 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,6 +13,18 @@ class Patient
     Appointment.new(date, self, docter)
   end
 
+  def appointments
+    Appointment.all.select do |appoint|
+      appoint.patient == self
+    end
+  end
+
+  def doctors
+    appointments.map do |appoint|
+      appoint.docter
+    end
+  end
+
   def self.all
     @@all
   end
